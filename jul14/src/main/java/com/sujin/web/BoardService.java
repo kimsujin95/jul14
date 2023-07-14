b46a5e3a75ad5638cdb5aaade9276169a9a333a3
soojin.kim@altiall.com <soojinkim!1>
2023-07-14 19:26:20 +0900

    MVC 3

---
+++ jul14/src/main/java/com/sujin/web/BoardService.java
@@ -0,0 +1,18 @@
+package com.sujin.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardDAO boardDAO;
+	
+	public List<BoardDTO> boardList() {
+		return boardDAO.boardList();
+	}
+	
+}
